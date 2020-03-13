98f461f1c7ecb400e82944fc6090c0b80b20eb2f
yashbellam97 <yashbellam97@gmail.com>
2020-03-13 11:21:34 -0400

    Change welcome and signup pages to activities

--- app/src/main/java/com/congregator/SignupActivity.java
+++ app/src/main/java/com/congregator/SignupActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class SignupActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcome);
+        setContentView(R.layout.activity_signup);
     }
 }
