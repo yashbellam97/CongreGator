79dba4d15d222c7f90e7e1485b28ce94c5f396ef
yashbellam97 <yashbellam97@gmail.com>
2020-03-13 14:34:50 -0400

    Change edittexts and theme to material design

--- app/src/main/java/com/congregator/WelcomeActivity.java
+++ app/src/main/java/com/congregator/WelcomeActivity.java
@@ -2,7 +2,10 @@ package com.congregator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -10,5 +13,23 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        Button loginButton = findViewById(R.id.activity_welcome_login_button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent loginActivityIntent = new Intent(view.getContext(), LoginActivity.class);
+                startActivity(loginActivityIntent);
+            }
+        });
+
+        Button signupButton = findViewById(R.id.activity_welcome_signup_button);
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent signupActivityIntent = new Intent(view.getContext(), SignupActivity.class);
+                startActivity(signupActivityIntent);
+            }
+        });
     }
 }
