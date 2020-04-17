08a2dced320515b0a2d7289e68a76e919b3dacb2
yashbellam97 <yashbellam97@gmail.com>
2020-04-17 16:11:22 -0400

    Add Toolbar to new walk activity

--- app/src/main/java/com/congregator/NewWalkActivity.java
+++ app/src/main/java/com/congregator/NewWalkActivity.java
@@ -3,6 +3,7 @@ package com.congregator;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class NewWalkActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class NewWalkActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_walk);
+
+        Toolbar newWalkToolbar = findViewById(R.id.activity_new_walk_app_bar);
+        setSupportActionBar(newWalkToolbar);
     }
 }
